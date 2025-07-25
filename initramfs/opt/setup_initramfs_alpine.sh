822f1ec877bf89d5f29d0a66ccd6b9a4fde53cf8
Sophia <193290223+soap-phia@users.noreply.github.com>
2025-07-25 07:03:24 -0400

    :3

--- initramfs/opt/setup_initramfs_alpine.sh
+++ initramfs/opt/setup_initramfs_alpine.sh
@@ -5,4 +5,4 @@ http://dl-cdn.alpinelinux.org/alpine/edge/main
 http://dl-cdn.alpinelinux.org/alpine/edge/community
 http://dl-cdn.alpinelinux.org/alpine/edge/testing
 EOF
-apk add bash e2fsprogs e2fsprogs-extra cloud-utils-growpart util-linux busybox parted grep cgpt coreutils sgdisk
+apk add bash e2fsprogs e2fsprogs-extra cloud-utils-growpart util-linux busybox parted grep cgpt coreutils sgdisk pv

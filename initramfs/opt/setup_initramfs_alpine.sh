5799a105f19e6a0144bacd8fc702ac6cc206605c
Sophia <193290223+soap-phia@users.noreply.github.com>
2025-08-01 19:00:29 -0400

    :3

--- initramfs/opt/setup_initramfs_alpine.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-cat <<EOF > /etc/apk/repositories
-http://dl-cdn.alpinelinux.org/alpine/edge/main
-http://dl-cdn.alpinelinux.org/alpine/edge/community
-http://dl-cdn.alpinelinux.org/alpine/edge/testing
-EOF
-apk add bash e2fsprogs e2fsprogs-extra cloud-utils-growpart util-linux busybox parted grep cgpt coreutils sgdisk pv rsync
